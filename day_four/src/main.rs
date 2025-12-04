c3e727ce65c04e94fa629a8e8d1f7f4c2a5acb3d
oliver greer <oliverscottgreer@gmail.com>
2025-12-03 22:52:10 -0800

    finished day 4

--- day_four/src/main.rs
+++ day_four/src/main.rs
@@ -4,6 +4,53 @@ use std::fs;
 // PART ONE:
 // ----------------
 
+// fn can_access(content: &Vec<Vec<char>>, x: i64, y: i64) -> bool {
+//     let directions: Vec<(i64, i64)> = vec![(1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1)];
+
+//     let mut total = 0;
+//     for dir in directions {
+//         if x+dir.0 >= 0 && y+dir.1 >= 0 && x+dir.0 <= (content.len() - 1)  as i64 && y+dir.1 <= (content[0].len() - 1) as i64 {
+//             if content[(x+dir.0) as usize][(y+dir.1) as usize] == '@' {
+//                 total += 1;
+//             }
+//         }
+//     }
+
+//     if total < 4 {
+//         return true;
+//     }
+
+//     false
+// }
+
+// fn main() {
+    
+//     let contents = fs::read_to_string("input.txt").expect("Does file not exist?");
+//     let mut sum: usize = 0;
+//     let mut content: Vec<Vec<char>> = Vec::new();
+
+//     for line in contents.lines() {
+//         content.push(line.chars().collect());
+//     }
+
+//     for x in 0..content.len() {
+//         for y in 0..content[0].len() {
+//             if content[x][y] == '@' {
+//                 if can_access(&content, x as i64, y as i64) {
+//                     sum += 1;
+//                 }
+//             }
+//         }
+//     }
+
+//     println!("{sum}");
+// }
+
+
+// ----------------
+// PART TWO:
+// ----------------
+
 fn can_access(content: &Vec<Vec<char>>, x: i64, y: i64) -> bool {
     let directions: Vec<(i64, i64)> = vec![(1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1)];
 
